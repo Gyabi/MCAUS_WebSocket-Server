a3eef6a966362b4e8c2ab40c7423007e0b9156f9
Gyabi <buyuuka12345@gmail.com>
2022-06-13 00:50:25 +0900

    python側で動作確認完了

--- client/test.py
+++ client/test.py
@@ -13,19 +13,19 @@ async def test_client():
         while True:
             json_data = json.dumps(send_data).encode()
             await websocket.send(json_data)
-            # print(await websocket.recv())
+            print(await websocket.recv())
             time.sleep(1)
 
 def create_message(first_data):
     send_data = {
     "connection-message":False,
     "id":first_data["id"],
-    "position-x":1.0,
-    "position-y":1.0,
-    "position-z":1.0,
-    "rotation-x":1.0,
-    "rotation-y":1.0,
-    "rotation-z":1.0
+    "position-x":0.0,
+    "position-y":0.0,
+    "position-z":0.0,
+    "rotation-x":0.0,
+    "rotation-y":0.0,
+    "rotation-z":0.0
     }
 
     return send_data
--- server/main.py
+++ server/main.py
@@ -1,26 +1,83 @@
-from fastapi import FastAPI
-from starlette.websockets import WebSocket
+import json
+from typing import Dict, List
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+import time
 
+# json構造
+# {
+#     "connection-message":True,
+#     "id":0,
+#     "position-x":0.0,
+#     "position-y":0.0,
+#     "position-z":0.0,
+#     "rotation-x":0.0,
+#     "rotation-y":0.0,
+#     "rotation-z":0.0
+# }
+
+# FaskAPIオブジェクト作成
 app = FastAPI()
+MAX_CONNECTION = 10
 
-# 接続中のクライアントを識別するためのIDを格納
-clients = {}
+# 接続管理用オブジェクト
+class ConnenctionManager:
+    def __init__(self) -> None:
+        self.active_connections : Dict = {}
+        self.object_ids : Dict = {}
+    
+    async def connect(self, websocket:WebSocket):
+        await websocket.accept()
+        # 既に10件の接続があれば拒否
+        if len(self.object_ids.values()) == MAX_CONNECTION:
+            await websocket.close()
+
+        # websocket接続キーをキーとする
+        key = websocket.headers.get('sec-websocket-key')
+        self.active_connections[key] = websocket
+
+        # 接続時にidを降る
+        for i in range(MAX_CONNECTION):
+            if i not in self.object_ids.values():
+                self.object_ids[key] = i
+                break
+    
+        send_data = {
+            "connection-message":True,
+            "id":self.object_ids[key]
+        }
+        # id送信
+        # time.sleep(1)
+        await self.send_personal_json_message(send_data,websocket)
+
+    def disconnect(self, websocket:WebSocket):
+        key = websocket.headers.get('sec-websocket-key')
+        del self.active_connections[key]
+        del self.object_ids[key]
+    
+    async def send_personal_json_message(self, json_message:Dict, websocket:WebSocket):
+        await websocket.send_json(json_message)
+
+    async def broadcast(self, json_message:Dict):
+        for connection in self.active_connections.values():
+            await connection.send_json(json_message)
+
+    async def broadcast_excluding_sender(self, json_message:Dict, websocket:WebSocket):
+        for connection in self.active_connections.values():
+            if connection != websocket:
+                await connection.send_json(json_message)
+
+manager = ConnenctionManager()
 
 # WebSockets用のエンドポイント
 @app.websocket("/ws")
-async def websocket_endpoint(ws: WebSocket):
-    await ws.accept()
-    # クライアントを識別するためのIDを取得
-    key = ws.headers.get('sec-websocket-key')
-    clients[key] = ws
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
     try:
         while True:
-            # クライアントからメッセージを受信
-            data = await ws.receive_text()
-            # 接続中のクライアントそれぞれにメッセージを送信（ブロードキャスト）
-            for client in clients.values():
-                await client.send_text(f"ID: {key} | Message: {data}")
-    except:
-        await ws.close()
-        # 接続が切れた場合、当該クライアントを削除する
-        del clients[key]
+            data = await websocket.receive_bytes()
+            data = json.loads(data)
+            print(data)
+            # await manager.send_personal_json_message({"responce":"true"}, websocket)
+            await manager.broadcast_excluding_sender(data, websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
